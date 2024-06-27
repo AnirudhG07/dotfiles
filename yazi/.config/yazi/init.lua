3cc4018cb5a3e8e1557f60c4766aa439263bfcb5
AnirudhG07 <anirudhgupta@iisc.ac.in>
2024-06-28 01:31:10 +0530

    more yazi plugins

--- yazi/.config/yazi/init.lua
+++ yazi/.config/yazi/init.lua
@@ -1,5 +1,7 @@
 require("full-border"):setup()
 -- ~/.config/yazi/init.lua
+require("relative-motions"):setup({ show_numbers = "relative", show_motion = true })
+-- ~/.config/yazi/init.lua
 require("bookmarks"):setup({
 	save_last_directory = false,
 	persist = "vim",
