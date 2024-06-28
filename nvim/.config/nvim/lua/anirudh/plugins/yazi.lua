788cfc0d070c0068d94b18fd6adc95e7201ba2f6
AnirudhG07 <anirudhgupta@iisc.ac.in>
2024-06-28 19:21:31 +0530

    yazi drag and drop
    header name
    
    user/group of files at the bottom
    
    yazi.nvim more

--- nvim/.config/nvim/lua/anirudh/plugins/yazi.lua
+++ nvim/.config/nvim/lua/anirudh/plugins/yazi.lua
@@ -1,11 +1,28 @@
 return {
-    "DreamMaoMao/yazi.nvim",
+    "mikavilpas/yazi.nvim",
     dependencies = {
-        "nvim-telescope/telescope.nvim",
         "nvim-lua/plenary.nvim",
     },
-
+    event = "VeryLazy",
     keys = {
-        { "<leader>yz", "<cmd>Yazi<CR>", desc = "Toggle Yazi" },
+        -- 👇 in this section, choose your own keymappings!
+        {
+            "<leader>yz",
+            function()
+                require("yazi").yazi()
+            end,
+            desc = "Open the file manager",
+        },
+        {
+            -- Open in the current working directory
+            "<leader>cw",
+            function()
+                require("yazi").yazi(nil, vim.fn.getcwd())
+            end,
+            desc = "Open the file manager in nvim's working directory",
+        },
+    },
+    opts = {
+        open_for_directories = false,
     },
 }
