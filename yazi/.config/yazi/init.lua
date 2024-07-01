431781bfc729bd0f2bd80022752adf4263dcc15f
AnirudhG07 <anirudhgupta@iisc.ac.in>
2024-07-01 16:14:43 +0530

    bookmarks.yazi delete

--- yazi/.config/yazi/init.lua
+++ yazi/.config/yazi/init.lua
@@ -1,21 +1,7 @@
 require("full-border"):setup()
 -- ~/.config/yazi/init.lua
 require("relative-motions"):setup({ show_numbers = "relative", show_motion = true })
--- ~/.config/yazi/init.lua
-require("bookmarks"):setup({
-	save_last_directory = false,
-	persist = "vim",
-	desc_format = "full",
-	notify = {
-		enable = true,
-		timeout = 5,
-		message = {
-			new = "New bookmark '<key>' -> '<folder>'",
-			delete = "Deleted bookmark in '<key>'",
-			delete_all = "Deleted all bookmarks",
-		},
-	},
-})
+
 require("keyjump"):setup({
 	icon_fg = "#fda1a1",
 })
