10111249aab77f4ca16be4148a913b5dd1e2f630
AnirudhG07 <anirudhgupta@iisc.ac.in>
2024-08-09 18:55:33 +0530

    yazi linemode add

--- yazi/.config/yazi/init.lua
+++ yazi/.config/yazi/init.lua
@@ -23,17 +23,16 @@ function Status:name()
 	return ui.Span(" " .. h.name .. linked)
 end
 
--- Header name
-function header_host()
+-- header name
+Header:children_add(function()
 	if ya.target_family() ~= "unix" then
 		return ui.Line({})
 	end
-	return ui.Span(ya.user_name() .. "@yazi" .. ": "):fg("blue")
-end
+	return ui.Span(ya.user_name() .. "@yazi" .. ":"):fg("blue")
+end, 500, Header.LEFT)
 
-Header:children_add(header_host, 500, Header.LEFT)
 -- group of files and username at the bottom
-function Status_owner()
+Status:children_add(function()
 	local h = cx.active.current.hovered
 	if h == nil or ya.target_family() ~= "unix" then
 		return ui.Line({})
@@ -45,6 +44,18 @@ function Status_owner()
 		ui.Span(ya.group_name(h.cha.gid) or tostring(h.cha.gid)):fg("magenta"),
 		ui.Span(" "),
 	})
-end
+end, 500, Status.RIGHT)
 
-Status:children_add(Status_owner, 500, Status.RIGHT)
+function Linemode:custom()
+	local year = os.date("%Y")
+	local time = (self._file.cha.modified or 0) // 1
+
+	if time > 0 and os.date("%Y", time) == year then
+		time = os.date("%b %d %H:%M", time)
+	else
+		time = time and os.date("%b %d  %Y", time) or ""
+	end
+
+	local size = self._file:size()
+	return ui.Line(string.format(" %s %s ", size and ya.readable_size(size):gsub(" ", "") or "-", time))
+end
