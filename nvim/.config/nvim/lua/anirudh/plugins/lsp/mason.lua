73938b865080c4b0743363bd8b45576af24f1cd3
AnirudhG07 <anirudhgupta@iisc.ac.in>
2024-09-05 14:16:42 +0530

    nvim copilot keybinding

--- nvim/.config/nvim/lua/anirudh/plugins/lsp/mason.lua
+++ nvim/.config/nvim/lua/anirudh/plugins/lsp/mason.lua
@@ -32,7 +32,6 @@ return {
 		mason_lspconfig.setup({
 			-- list of servers for mason to install
 			ensure_installed = {
-				"tsserver",
 				"html",
 				"cssls",
 				"tailwindcss",
@@ -43,7 +42,6 @@ return {
 				"prismals",
 				"pyright",
 				"rust_analyzer",
-				"ruff server",
 			},
 		})
 
