48fc2536ccb5e4646d050373423d7502d2bf0d52
AnirudhG07 <anirudhgupta@iisc.ac.in>
2024-10-15 01:09:25 +0530

    noice and notification fixed

--- nvim/.config/nvim/lua/anirudh/plugins/nvim-notify.lua
+++ nvim/.config/nvim/lua/anirudh/plugins/nvim-notify.lua
@@ -2,10 +2,8 @@ return {
 	"rcarriga/nvim-notify",
 	config = function()
 		require("notify").setup({
-			stages = "fade",
-			timeout = 5000,
-			background_colour = "blue", -- blue color
-			text_colour = "black",
+			stages = "fade_in_slide_out",
+			timeout = 500,
 			icons = {
 				ERROR = "",
 				WARN = "",
