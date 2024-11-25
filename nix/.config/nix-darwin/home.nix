84e0da797b33bae1369d6cc038618f55295d8845
AnirudhG07 <anirudhgupta@iisc.ac.in>
2024-11-25 17:50:06 +0530

    i want home.nix not for my dotfiles.
    not finding it nice to do home-manager switch everytime.

--- nix/.config/nix-darwin/home.nix
+++ nix/.config/nix-darwin/home.nix
@@ -12,17 +12,17 @@
     home.packages = [];
 
     # Home Manager is pretty good at managing dotfiles. The primary way to manage
-    # plain files is through 'home.file'.
+    # plain files is through 'home.file'. I am using stow for instant changes visible. This below WORKS!!!
     home.file = {
     ".zshrc".source = ../../../zsh/.zshrc;
-    ".config/yazi".source = ../../../yazi/.config/yazi;
-    ".p10k.zsh".source = ../../../p10k.zsh/.p10k.zsh;
-    ".config/starship.toml".source = ../../../starship/.config/starship.toml;
-    ".config/borders".source = ../../../borders/.config/borders;
-    ".config/nvim".source = ../../../nvim/.config/nvim;
-    ".config/nix".source = ../../../nix/.config/nix;
-    ".config/nix-darwin".source = ../../../nix/.config/nix-darwin;
-    ".config/aerospace".source = ../../../aerospace/.config/aerospace;
+    #".config/yazi".source = ../../../yazi/.config/yazi;
+    #".p10k.zsh".source = ../../../p10k.zsh/.p10k.zsh;
+    #".config/starship.toml".source = ../../../starship/.config/starship.toml;
+    #".config/borders".source = ../../../borders/.config/borders;
+    #".config/nvim".source = ../../../nvim/.config/nvim;
+    #".config/nix".source = ../../../nix/.config/nix;
+    #".config/nix-darwin".source = ../../../nix/.config/nix-darwin;
+    #".config/aerospace".source = ../../../aerospace/.config/aerospace;
     #".tmux.conf".source = ../../../tmux/.tmux.conf;
     #".config/aerospace".source = ../../../aerospace/.config/aerospace;
     # ".config/yabai".source = "../../../yabai;
