82a453ae0e7ea3b0ba36d25ee1a6187979a46a77
AnirudhG07 <anirudhgupta@iisc.ac.in>
2024-12-10 23:06:02 +0530

    yazi0.4 more updates

--- yazi/.config/yazi/plugins/glow.yazi/init.lua
+++ yazi/.config/yazi/plugins/glow.yazi/init.lua
@@ -1,13 +1,13 @@
 local M = {}
 
-function M:peek(job)
+function M:peek()
 	local child = Command("glow")
 		:args({
 			"--style",
 			"dark",
 			"--width",
-			tostring(job.area.w),
-			tostring(job.file.url),
+			tostring(self.area.w),
+			tostring(self.file.url),
 		})
 		:env("CLICOLOR_FORCE", "1")
 		:stdout(Command.PIPED)
@@ -15,36 +15,36 @@ function M:peek(job)
 		:spawn()
 
 	if not child then
-		return require("code").peek(job)
+		return require("code").peek(self)
 	end
 
-	local limit = job.area.h
+	local limit = self.area.h
 	local i, lines = 0, ""
 	repeat
 		local next, event = child:read_line()
 		if event == 1 then
-			return require("code").peek(job)
+			return require("code").peek(self)
 		elseif event ~= 0 then
 			break
 		end
 
 		i = i + 1
-		if i > job.skip then
+		if i > self.skip then
 			lines = lines .. next
 		end
-	until i >= job.skip + limit
+	until i >= self.skip + limit
 
 	child:start_kill()
-	if job.skip > 0 and i < job.skip + limit then
-		ya.manager_emit("peek", { math.max(0, i - limit), only_if = job.file.url, upper_bound = true })
+	if self.skip > 0 and i < self.skip + limit then
+		ya.manager_emit("peek", { math.max(0, i - limit), only_if = self.file.url, upper_bound = true })
 	else
 		lines = lines:gsub("\t", string.rep(" ", PREVIEW.tab_size))
-		ya.preview_widgets(job, { ui.Text.parse(lines):area(job.area) })
+		ya.preview_widgets(self, { ui.Text.parse(lines):area(self.area) })
 	end
 end
 
-function M:seek(job)
-	require("code").seek(job, job.units)
+function M:seek(units)
+	require("code").seek(self, units)
 end
 
 return M
