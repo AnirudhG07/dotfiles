4fb35f9340ab85805d70bd327b6efa69fe04f564
AnirudhG07 <anirudhgupta@iisc.ac.in>
2025-01-13 16:24:02 +0530

    nix upgrade with more packages

--- nix/.config/home-manager/flake.nix
+++ nix/.config/home-manager/flake.nix
@@ -1,19 +1,80 @@
 {
-  description = "Home Manager configuration of anirudhgupta";
+    description = "Anirudh Gupta's Nix-darwin System Flake";
 
-  inputs = {
-    # Specify the source of Home Manager and Nixpkgs.
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+        nix-darwin.url = "github:LnL7/nix-darwin";
+        nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+        home-manager = {
+            url = "github:nix-community/home-manager";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+
+        nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
+
+        homebrew-core = {
+            url = "github:homebrew/homebrew-core";
+            flake = false;
+        };
+        homebrew-cask = {
+            url = "github:homebrew/homebrew-cask";
+            flake = false;
+        };
+        homebrew-bundle = {
+            url = "github:homebrew/homebrew-bundle";
+            flake = false;
+        };
     };
-  };
 
-  outputs = { nixpkgs, home-manager, ... }:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
+    outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager, nix-homebrew, homebrew-core, homebrew-cask, homebrew-bundle, ...}:
+        let
+          system = "x86_64-linux";
+          pkgs = nixpkgs.legacyPackages.${system};
+
+        configuration = { pkgs, config, ... }: {
+        # List packages installed in system profile. To search by name, run:
+        # $ nix-env -qaP | grep wget
+
+            nixpkgs.config.allowUnfree = true;
+
+            environment.systemPackages = with pkgs; [
+                neovim
+                stow
+                gh
+                lazygit
+            ];
+
+            # home-manager
+            users.users.anirudhgupta = {
+                name = "anirudhgupta";
+                home = "/home/anirudhgupta";
+                shell = pkgs.zsh;
+            };
+
+            homebrew = {
+                enable = true;
+                brews = [
+                    "starship"
+                ];
+                casks = [
+                ];
+            };
+
+
+            # Necessary for using flakes on this system.
+            nix.settings.experimental-features = "nix-command flakes";
+            # Set Git commit hash for darwin-version.
+            system.configurationRevision = self.rev or self.dirtyRev or null;
+
+            # $ darwin-rebuild changelog
+            system.stateVersion = 5;
+
+            # The platform the configuration will be used on.
+
+            programs.zsh = {
+                enable = true;
+            };
+        };
     in {
       homeConfigurations."anirudhgupta" = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
--- nix/.config/home-manager/home-packages.nix
+++ nix/.config/home-manager/home-packages.nix
@@ -3,24 +3,46 @@
   nixpkgs.config.allowUnfree = true;
 
   home.packages = with pkgs; [
+    # Core Utils
+    tmux
+    fzf
+    zoxide
+    ripgrep
+    fd
+    bat
+    eza
+    imagemagick
+    direnv
+
     # CLI tools
-    _7zz
-    aria2
-    btop
-    chafa
-    coreutils
-    delta
-    fabric-ai
-    ffmpeg
-    glow
+    _7zz 
+    aria2 # wget replacement
+    btop # Better top
+    chafa # For terminal image preview
+    coreutils # GNU core utilities
+    delta # Git diff viewer
+    fabric-ai # terminal AI utility
+    ffmpeg # image/video processing
+    glow # markdown viewer
+    ncdu # disk usage analyzer
+    ouch # archive manager
+    rich-cli # rich previewing
+    tldr # Too Long; Didn't Read
+    ueberzugpp # image previewer
+    television # Better fzf
+    krabby # Pokemons
+
+    # languages
+    ## Python
+    python313
+    uv # package manager
+    ruff # python linter
+
     go
     lua
-    ncdu
-    ouch
-    rich-cli
-    ruff
-    rustup
-    tldr
-    television
-  ];
+    ## Lean4
+    elan
+    ## Rust
+    rustup # rust toolchain manager
+    ];
 }
